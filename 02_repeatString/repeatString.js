7dc0b23b8608b2c2a11f2d49ed387d5e515b159f
Marwan Esam <competitiveflamingo@gmail.com>
2024-06-17 02:35:34 +0300

    Return repeated string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+    let concatenatedString = "";
+    for(let i = 1 ; i <= num ; i++){
+        concatenatedString += string;
+    }
+    return concatenatedString;
 };
 
 // Do not edit below this line
