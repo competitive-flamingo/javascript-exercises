8338ddd5c13d6ca74021d40acfc20b9bec74b609
Marwan Esam <competitiveflamingo@gmail.com>
2024-06-17 03:59:46 +0300

    Sum between begin & end (included)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    const swap = (begin, end) => [end, begin];
+    if(begin < 0 || end < 0 || !Number.isInteger(begin) || !Number.isInteger(end)) return "ERROR";
+    if(begin > end) [begin, end] = swap(begin, end);
+    let sum = 0;
+    for(let i = begin ; i <= end ; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
