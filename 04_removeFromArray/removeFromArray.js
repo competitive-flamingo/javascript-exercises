cb224f68a0c73033747863b8957e4cb4558e8650
Marwan Esam <competitiveflamingo@gmail.com>
2024-06-17 03:27:32 +0300

    Remove existing argument values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+    function removeElements(val) {
+        return !elementsToRemove.includes(val);
+    }
+    return arr.filter(removeElements);
 };
 
 // Do not edit below this line
