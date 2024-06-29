49090ab1e8530cbdf5777c2523fe42b9287807f7
Marwan Esam <competitiveflamingo@gmail.com>
2024-06-29 23:30:31 +0300

    Check palindrome strings

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const mappedString = string.split("").map((char) => char.toUpperCase());
+    const filteredString = mappedString.filter((char) => isAllowed(char));
+    return isPalindrome(filteredString);
+    function isAllowed(char) {
+        return (char >= "A" && char <= "Z") || (char >= "0" && char <= "9");
+    }
+    function isPalindrome(arr) {
+        for(let i = 0 ; i < arr.length / 2 ; i++) {
+            if(arr[i] !== arr[arr.length - i - 1]) return false;
+        }
+        return true;
+    }
 };
 
 // Do not edit below this line
