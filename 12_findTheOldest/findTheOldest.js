9a033f36466e9ea4824fb2f5687e35a2ab770c7f
Marwan Esam <competitiveflamingo@gmail.com>
2024-06-30 00:19:33 +0300

    Get oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    function getAge(birthYear, deathYear) {
+        if(!deathYear) deathYear = new Date().getFullYear();
+        return deathYear - birthYear;
+    }
+    return people.reduce((oldest, person) => {
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
