f15b1ac2e0519a8ff068bc41f239a7e65dbc0d68
Marwan Esam <competitiveflamingo@gmail.com>
2024-06-29 23:49:32 +0300

    Get fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = typeof num === "number" ? num : Number(num);
+    if(num < 0) return "OOPS";
+    let prev = 0;
+    let current = 1;
+    for(let i = 1 ; i <= num ; i++) {
+        let next = prev + current;
+        prev = current;
+        current = next;
+    }
+    return prev;
 };
 
 // Do not edit below this line
